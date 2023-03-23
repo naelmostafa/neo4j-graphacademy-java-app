836a82934a15190dd6a29766454234847d024088
Nael Mostafa <naelnael53.nm@gmail.com>
2023-03-23 16:58:53 +0200

    _02_MovieList
    - Open session
    - Excute Query in new transacion
    - List all movies from transaction result
    - Tests run: 3

--- src/main/java/neoflix/services/MovieService.java
+++ src/main/java/neoflix/services/MovieService.java
@@ -5,6 +5,7 @@ import neoflix.NeoflixApp;
 import neoflix.Params;
 import org.neo4j.driver.Driver;
 import org.neo4j.driver.TransactionContext;
+import org.neo4j.driver.Value;
 import org.neo4j.driver.Values;
 
 import java.util.HashMap;
@@ -13,7 +14,7 @@ import java.util.Map;
 
 public class MovieService {
     private final Driver driver;
-    private final List<Map<String,Object>> popular;
+    private final List<Map<String, Object>> popular;
     private final List<Map<String, Object>> directedByCoppola;
     private final List<Map<String, Object>> actedInTomHanks;
     private final List<Map<String, Object>> comedyMovies;
@@ -32,10 +33,12 @@ public class MovieService {
 
     /**
      * This method should return a paginated list of movies ordered by the `sort`
-     * parameter and limited to the number passed as `limit`.  The `skip` variable should be
+     * parameter and limited to the number passed as `limit`. The `skip` variable
+     * should be
      * used to skip a certain number of rows.
      *
-     * If a userId value is supplied, a `favorite` boolean property should be returned to
+     * If a userId value is supplied, a `favorite` boolean property should be
+     * returned to
      * signify whether the user has aded the movie to their "My Favorites" list.
      *
      * @param params query params (query, sort, order, limit, skip)
@@ -43,32 +46,51 @@ public class MovieService {
      * @returns {Promise<Record<string, any>[]>}
      */
     // tag::all[]
-    public List<Map<String,Object>> all(Params params, String userId) {
+    public List<Map<String, Object>> all(Params params, String userId) {
         // TODO: Open an Session
         // TODO: Execute a query in a new Read Transaction
         // TODO: Get a list of Movies from the Result
         // TODO: Close the session
 
-        return AppUtils.process(popular, params);
+        try (var session = driver.session()) {
+            var movies = session.executeRead(transaction -> {
+                Params.Sort sort = params.sort(Params.Sort.title);
+                String query = String.format("""
+                        MATCH (m:Movie)
+                        WHERE m.`%s` IS NOT NULL
+                        RETURN  m{.*} AS movie
+                        ORDER BY m.`%s` %s
+                        SKIP $skip
+                        LIMIT $limit
+                        """, sort, sort, params.order());
+                var result = transaction.run(query, Values.parameters("skip", params.skip(), "limit", params.limit()));
+                return result.list(row -> row.get("movie").asMap());
+            });
+            return movies;
+        }
     }
     // end::all[]
 
-
     /**
      * @public
-     * This method find a Movie node with the ID passed as the `id` parameter.
-     * Along with the returned payload, a list of actors, directors, and genres should
-     * be included.
-     * The number of incoming RATED relationships should also be returned as `ratingCount`
+     *         This method find a Movie node with the ID passed as the `id`
+     *         parameter.
+     *         Along with the returned payload, a list of actors, directors, and
+     *         genres should
+     *         be included.
+     *         The number of incoming RATED relationships should also be returned as
+     *         `ratingCount`
      *
-     * If a userId value is suppled, a `favorite` boolean property should be returned to
-     * signify whether the user has aded the movie to their "My Favorites" list.
+     *         If a userId value is suppled, a `favorite` boolean property should be
+     *         returned to
+     *         signify whether the user has aded the movie to their "My Favorites"
+     *         list.
      *
      * @param {string} id
      * @returns {Promise<Record<string, any>>}
      */
     // tag::findById[]
-    public Map<String,Object> findById(String id, String userId) {
+    public Map<String, Object> findById(String id, String userId) {
         // TODO: Find a movie by its ID
         // MATCH (m:Movie {tmdbId: $id})
 
@@ -77,16 +99,20 @@ public class MovieService {
     // end::findById[]
 
     /**
-     * This method should return a paginated list of similar movies to the Movie with the
-     * id supplied.  This similarity is calculated by finding movies that have many first
+     * This method should return a paginated list of similar movies to the Movie
+     * with the
+     * id supplied. This similarity is calculated by finding movies that have many
+     * first
      * degree connections in common: Actors, Directors and Genres.
      *
-     * Results should be ordered by the `sort` parameter, and in the direction specified
+     * Results should be ordered by the `sort` parameter, and in the direction
+     * specified
      * in the `order` parameter.
      * Results should be limited to the number passed as `limit`.
      * The `skip` variable should be used to skip a certain number of rows.
      *
-     * If a userId value is suppled, a `favorite` boolean property should be returned to
+     * If a userId value is suppled, a `favorite` boolean property should be
+     * returned to
      * signify whether the user has aded the movie to their "My Favorites" list.
      *
      * @param id
@@ -95,29 +121,31 @@ public class MovieService {
      * @returns List<Movie> similar movies
      */
     // tag::getSimilarMovies[]
-    public List<Map<String,Object>> getSimilarMovies(String id, Params params, String userId) {
+    public List<Map<String, Object>> getSimilarMovies(String id, Params params, String userId) {
         // TODO: Get similar movies based on genres or ratings
 
         return AppUtils.process(popular, params).stream()
                 .map(item -> {
-                    Map<String,Object> copy = new HashMap<>(item);
-                    copy.put("score", ((int)(Math.random() * 10000)) / 100.0);
+                    Map<String, Object> copy = new HashMap<>(item);
+                    copy.put("score", ((int) (Math.random() * 10000)) / 100.0);
                     return copy;
                 }).toList();
     }
     // end::getSimilarMovies[]
 
-
     /**
-     * This method should return a paginated list of movies that have a relationship to the
+     * This method should return a paginated list of movies that have a relationship
+     * to the
      * supplied Genre.
      *
-     * Results should be ordered by the `sort` parameter, and in the direction specified
+     * Results should be ordered by the `sort` parameter, and in the direction
+     * specified
      * in the `order` parameter.
      * Results should be limited to the number passed as `limit`.
      * The `skip` variable should be used to skip a certain number of rows.
      *
-     * If a userId value is supplied, a `favorite` boolean property should be returned to
+     * If a userId value is supplied, a `favorite` boolean property should be
+     * returned to
      * signify whether the user has added the movie to their "My Favorites" list.
      *
      * @param name
@@ -126,7 +154,7 @@ public class MovieService {
      * @return List<Movie> movies for that genre
      */
     // tag::getByGenre[]
-    public List<Map<String,Object>> byGenre(String name, Params params, String userId) {
+    public List<Map<String, Object>> byGenre(String name, Params params, String userId) {
         // TODO: Get Movies in a Genre
         // MATCH (m:Movie)-[:IN_GENRE]->(:Genre {name: $name})
 
@@ -135,24 +163,27 @@ public class MovieService {
     // end::getByGenre[]
 
     /**
-     * This method should return a paginated list of movies that have an ACTED_IN relationship
+     * This method should return a paginated list of movies that have an ACTED_IN
+     * relationship
      * to a Person with the id supplied
      *
-     * Results should be ordered by the `sort` parameter, and in the direction specified
+     * Results should be ordered by the `sort` parameter, and in the direction
+     * specified
      * in the `order` parameter.
      * Results should be limited to the number passed as `limit`.
      * The `skip` variable should be used to skip a certain number of rows.
      *
-     * If a userId value is suppled, a `favorite` boolean property should be returned to
+     * If a userId value is suppled, a `favorite` boolean property should be
+     * returned to
      * signify whether the user has aded the movie to their "My Favorites" list.
      *
      * @param actorId actor id
-     * @param params query params with sorting and pagination
+     * @param params  query params with sorting and pagination
      * @param userId
      * @return List<Movie>
      */
     // tag::getForActor[]
-    public List<Map<String,Object>> getForActor(String actorId, Params params,String userId) {
+    public List<Map<String, Object>> getForActor(String actorId, Params params, String userId) {
         // TODO: Get Movies acted in by a Person
         // MATCH (:Person {tmdbId: $id})-[:ACTED_IN]->(m:Movie)
 
@@ -161,24 +192,27 @@ public class MovieService {
     // end::getForActor[]
 
     /**
-     * This method should return a paginated list of movies that have an DIRECTED relationship
+     * This method should return a paginated list of movies that have an DIRECTED
+     * relationship
      * to a Person with the id supplied
      *
-     * Results should be ordered by the `sort` parameter, and in the direction specified
+     * Results should be ordered by the `sort` parameter, and in the direction
+     * specified
      * in the `order` parameter.
      * Results should be limited to the number passed as `limit`.
      * The `skip` variable should be used to skip a certain number of rows.
      *
-     * If a userId value is suppled, a `favorite` boolean property should be returned to
+     * If a userId value is suppled, a `favorite` boolean property should be
+     * returned to
      * signify whether the user has aded the movie to their "My Favorites" list.
      *
      * @param directorId director id
-     * @param params query params with sorting and pagination
+     * @param params     query params with sorting and pagination
      * @param userId
      * @return List<Movie>
      */
     // tag::getForDirector[]
-    public List<Map<String,Object>> getForDirector(String directorId, Params params,String userId) {
+    public List<Map<String, Object>> getForDirector(String directorId, Params params, String userId) {
         // TODO: Get Movies directed by a Person
         // MATCH (:Person {tmdbId: $id})-[:DIRECTED]->(m:Movie)
 
@@ -186,12 +220,11 @@ public class MovieService {
     }
     // end::getForDirector[]
 
-
     /**
      * This function should return a list of tmdbId properties for the movies that
      * the user has added to their 'My Favorites' list.
      *
-     * @param tx The open transaction
+     * @param tx     The open transaction
      * @param userId The ID of the current user
      * @return List<String> movieIds of favorite movies
      */
@@ -201,5 +234,6 @@ public class MovieService {
     }
     // end::getUserFavorites[]
 
-    record Movie() {} // todo
+    record Movie() {
+    } // todo
 }
